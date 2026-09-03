8be8615e1b33319a42bc5c21900982a651f11fbc
Arnold Adez <adeztech2@gmail.com>
2026-09-03 17:40:51 +0300

    Add package.json for project configuration

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+"name": "adez-tech-vpn-store",
+"version": "1.0.0",
+"description": "ADEZ TECH VPN Store",
+"type": "commonjs",
+"main": "server.js",
+"scripts": {
+"start": "node server.js",
+"dev": "node server.js"
+},
+"engines": {
+"node": "20.x"
+},
+"dependencies": {
+"express": "^4.21.2"
+}
+}

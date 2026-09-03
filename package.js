f24412485c127a0885d1205bde3befdf83fc8156
Arnold Adez <adeztech2@gmail.com>
2026-09-03 17:57:49 +0300

    Fix formatting and remove duplicate script entry

--- package.js
+++ package.js
@@ -1,17 +1,16 @@
 {
-"name": "adez-tech-vpn-store",
-"version": "1.0.0",
-"description": "ADEZ TECH VPN Store",
-"main": "server.js",
-"type": "commonjs",
-"scripts": {
-"start": "node server.js",
-"dev": "node server.js"
-},
-"engines": {
-"node": "20.x"
-},
-"dependencies": {
-"express": "^4.21.2"
-}
+  "name": "adez-tech-vpn-store",
+  "version": "1.0.0",
+  "description": "ADEZ TECH VPN Store",
+  "main": "server.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "engines": {
+    "node": "20.x"
+  },
+  "dependencies": {
+    "express": "^4.21.2"
+  }
 }
